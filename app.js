03d4f6e56a25888a226a264f7ac27886b94b5217
devleon <devleon00@gmail.com>
2022-05-11 13:10:31 -0500

    Add my express server

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/launchx', (req, res) => {
+    res.send('Bienvenidos a launchx')
+})
+
+app.get('/explorersInNode', (req, res) => {
+    const explorer = {name: "EXplorer", msg: "Hello"}
+    res.send(explorer)
+})
+
+app.get('/explorers/:explorerName', (req, res) => {
+    res.send(req.params)
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
